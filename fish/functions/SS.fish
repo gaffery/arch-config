6e4c9e729c48df64f42608eed4c26baa4971e638
gaffey <jiangze212@gmail.com>
2018-09-24 23:08:55 +0800

    Daily update

--- fish/functions/SS.fish
+++ fish/functions/SS.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 0
-function SS --description 'alias SS proxychains4'
-	proxychains4  $argv;
+function SS --description 'alias SS proxychains4 -f ~/.config/privarcy/etc/proxychains.conf'
+	proxychains4 -f ~/.config/privarcy/etc/proxychains.conf $argv;
 end
